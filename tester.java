4f5cec0e0a5b2e172054c390eee32c1fda350bfc
ayushs2725 <aysingh@ucsd.edu>
2022-04-30 15:40:32 -0700

    ljkn

---
+++ tester.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class tester 
+{
+    @Test
+    public void fail()
+    {
+        assertEquals(9, test.multiply(3, 3));
+    }
+    
+}
